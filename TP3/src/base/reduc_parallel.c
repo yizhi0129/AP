bbb2e8bc1de0c03732947ce31954258a72b862e2
yaspr <yaspr@pm.me>
2023-11-25 15:45:10 +0100

    Updating TP3 with new codes

--- TP3/src/base/reduc_parallel.c
+++ TP3/src/base/reduc_parallel.c
@@ -2,6 +2,7 @@
 #define _GNU_SOURCE
 
 //
+#include <omp.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -81,26 +82,44 @@ f64 reduc_parallel(f64 *restrict a, u64 n, u64 nt)
   //Mask for thread pinning
   cpu_set_t cpuset;
   
+<<<<<<< HEAD:TP3/src/reduc_parallel.c
   //Allocating threads data structure
   thread_data_t *td = malloc(sizeof(thread_data_t) * nt);
 
+=======
+  //Allocating threads data array
+  thread_data_t **td = malloc(sizeof(thread_data_t *) * nt);
+  
+>>>>>>> ee701ec (Updating TP3 with new codes):TP3/src/base/reduc_parallel.c
   if (!td)
     {
       printf("Error: cannot allocate thread data\n");
       exit(-1);
     }
 
+  //
+  f64 n_mod = (n % nt);
+  f64 n_div = (n / nt);
+  
   //Creating and pinning threads
   //creation, allocation, distrubution of charges
   for (u64 i = 0; i < nt; i++)
     {
+<<<<<<< HEAD:TP3/src/reduc_parallel.c
       //Clear cpuset mask (64 bits), initialized 0
+=======
+      //Allocate thread data entry in the array
+      td[i] = malloc(sizeof(thread_data_t));
+      
+      //Clear cpuset mask
+>>>>>>> ee701ec (Updating TP3 with new codes):TP3/src/base/reduc_parallel.c
       CPU_ZERO(&cpuset);
       
       //Setting up the target CPU core, thread i to core i
       CPU_SET(i, &cpuset);
 
       //Number of elements per thread. 
+<<<<<<< HEAD:TP3/src/reduc_parallel.c
       // add cases where n % nt != 0
       td[i].n = (n / nt) + (i < n % nt ? 1 : 0); //block size
       td[i].a = a + i * (n / nt + (i < n % nt ? 1 : 0)); //pointer to the beginning of the block 
@@ -108,13 +127,27 @@ f64 reduc_parallel(f64 *restrict a, u64 n, u64 nt)
       
       //Create the thread
       pthread_create(&td[i].id, NULL, _reduc_, &td[i]); //&td[i] : parameter of _reduc_
+=======
+      td[i]->n = n_div + (n_mod != 0);
+      td[i]->a = a + (i * td[i]->n);
+      td[i]->r = 0.0;
+      
+      //Create the thread
+      pthread_create(&td[i]->id, NULL, _reduc_, td[i]);
+>>>>>>> ee701ec (Updating TP3 with new codes):TP3/src/base/reduc_parallel.c
 
       //Pin the thread on the previously set up core 
-      pthread_setaffinity_np(td[i].id, sizeof(cpuset), &cpuset);
+      pthread_setaffinity_np(td[i]->id, sizeof(cpuset), &cpuset);
 
+<<<<<<< HEAD:TP3/src/reduc_parallel.c
       //
       printf("thread id: %llu; core: %llu\n", (u64)td[i].id, i);
     } 
+=======
+      if (n_mod)
+	n_mod--;
+    }
+>>>>>>> ee701ec (Updating TP3 with new codes):TP3/src/base/reduc_parallel.c
   
   //barrior: waiting 
   //clone: command to create a thread
@@ -123,12 +156,20 @@ f64 reduc_parallel(f64 *restrict a, u64 n, u64 nt)
   // partial sum -> sum
   for (u64 i = 0; i < nt; i++)
     {
-      pthread_join(td[i].id, NULL);
+      pthread_join(td[i]->id, NULL);
 
-      r += td[i].r;
+      r += td[i]->r;
+      
+      free(td[i]);
     }
+<<<<<<< HEAD:TP3/src/reduc_parallel.c
   //mutex : mutual exclusion
   //to realize atomicity here
+=======
+
+  free(td);
+  
+>>>>>>> ee701ec (Updating TP3 with new codes):TP3/src/base/reduc_parallel.c
   return r;
 }
 
@@ -153,32 +194,58 @@ int main(int argc, char **argv)
       printf("Error: number threads is 0\n");
       exit(-1);
     }
-
+  
   //Size in bytes
   u64 s = n * sizeof(f64);
-  
-  //
-  f64 *restrict a = aligned_alloc(32, s);
-
-  init(a, n, 'c');
-  
-  f64 rs  = reduc_sequential(a, n);
-  f64 rp  = reduc_parallel(a, n, nt);
 
   //
-  printf("allocated memory : %llu B, %llu KiB, %llu MiB, %llu GiB\n",
+  printf("\nallocated memory  : %llu B, %llu KiB, %llu MiB, %llu GiB\n",
 	 s,
 	 s >> 10,
 	 s >> 20,
 	 s >> 30);
-  
-  printf("sequential result: %lf\n", rs);
-  printf("parallel result  : %lf\n", rp);
 
-  f64 delta = fabs(rs - rp);
+  //
+  f64 t1 = 0.0, t2 = 0.0;
   
+<<<<<<< HEAD:TP3/src/reduc_parallel.c
   //verify precision problem
   printf("delta            : %lf (%e)\n", delta, delta);
+=======
+  //
+  f64 *restrict a = aligned_alloc(64, s);
+
+  init(a, n, 'c');
+
+  //Sequential
+  t1 = omp_get_wtime();
+  
+  f64 rs  = reduc_sequential(a, n);
+
+  t2 = omp_get_wtime();  
+
+  f64 elapsed_s = (f64)(t2 - t1);
+
+  //Parallel
+  t1 = omp_get_wtime();
+  
+  f64 rp  = reduc_parallel(a, n, nt);
+
+  t2 = omp_get_wtime();
+
+  f64 elapsed_p = (f64)(t2 - t1);
+    
+  printf("\nsequential result : %lf\n", rs);
+  printf("sequential elapsed: %.5lf s\n", elapsed_s);
+  printf("\nparallel result   : %lf\n", rp);
+  printf("parallel elapsed  : %.5lf s\n", elapsed_p);
+  
+  f64 delta = fabs(rs - rp);
+  f64 speedup = (elapsed_s / elapsed_p);
+  
+  printf("\nresults delta      : %lf (%e)\n", delta, delta);
+  printf("speedup           : %.3lf\n", speedup);
+>>>>>>> ee701ec (Updating TP3 with new codes):TP3/src/base/reduc_parallel.c
   
   //
   free(a);
